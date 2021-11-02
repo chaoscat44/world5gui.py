9757e7f8739bf7aaa19fc3520426523669bd6e62
chaoscat44 <80566343+chaoscat44@users.noreply.github.com>
2021-11-02 12:28:21 +0100

    Update window.py

--- window.py
+++ window.py
@@ -1,6 +1,27 @@
 #!/usr/bin/python3
 
-import tkinter # note that module name has changed from Tkinter in Python 2 to tkinter in Python 3
-top = tkinter.Tk()
-# Code to add widgets will go here...
-top.mainloop()
+from tkinter import *
+
+class Window(Frame):
+
+    def __init__(self, master=None):
+        Frame.__init__(self, master)        
+        self.master = master
+
+        # widget can take all window
+        self.pack(fill=BOTH, expand=1)
+
+        # create button, link it to clickExitButton()
+        exitButton = Button(self, text="Exit", command=self.clickExitButton)
+
+        # place button at (0,0)
+        exitButton.place(x=0, y=0)
+
+    def clickExitButton(self):
+        exit()
+        
+root = Tk()
+app = Window(root)
+root.wm_title("Tkinter button")
+root.geometry("320x200")
+root.mainloop()
