66158db4a1af1369518781dcb0c6095d2bdd5483
chaoscat44 <80566343+chaoscat44@users.noreply.github.com>
2021-11-02 13:10:40 +0100

    Update window.py

--- window.py
+++ window.py
@@ -3,25 +3,28 @@
 from tkinter import *
 
 class Window(Frame):
-
     def __init__(self, master=None):
-        Frame.__init__(self, master)        
+        Frame.__init__(self, master)
         self.master = master
 
-        # widget can take all window
-        self.pack(fill=BOTH, expand=1)
+        menu = Menu(self.master)
+        self.master.config(menu=menu)
 
-        # create button, link it to clickExitButton()
-        exitButton = Button(self, text="Exit", command=self.clickExitButton)
+        fileMenu = Menu(menu)
+        fileMenu.add_command(label="Item")
+        fileMenu.add_command(label="Exit", command=self.exitProgram)
+        menu.add_cascade(label="File", menu=fileMenu)
 
-        # place button at (0,0)
-        exitButton.place(x=0, y=0)
-
-    def clickExitButton(self):
+        editMenu = Menu(menu)
+        editMenu.add_command(label="Undo")
+        editMenu.add_command(label="Redo")
+        menu.add_cascade(label="Edit", menu=editMenu)
+        
+	
+    def exitProgram(self):
         exit()
         
 root = Tk()
 app = Window(root)
-root.wm_title("Tkinter button")
-root.geometry("320x200")
+root.wm_title("world5")
 root.mainloop()
